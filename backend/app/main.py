0dfed310b37e38c6bd26d1ef7150f1eb952cf748
khushitapanchal <khushita1510@gmail.com>
2026-04-08 11:03:46 +0530

    backend main file is updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,10 @@ app = FastAPI(title="Salon Customer Management System API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://salonn-six.vercel.app",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
