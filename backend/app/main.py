240f1effa68004a44d594bb316952d637535ca86
khushitapanchal <khushita1510@gmail.com>
2026-04-27 14:51:29 +0530

    backend fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
         "http://localhost:3000",
         "https://salonn-six.vercel.app",
     ],
+    allow_origin_regex=r"https://salonn-.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
